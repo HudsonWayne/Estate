8cafdba8aed9be670568beb23fe97302545608b4
HudsonWayne <waynewaynebenhura@gmail.com>
2024-06-25 13:56:33 +0200

    modelling

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,33 +1,29 @@
 import bcrypt from "bcrypt";
 import prisma from "../lib/prisma.js";
 
+const SALT_ROUNDS = 10; // Adjust cost factor as needed
 
+export const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
 
-export const register = async (req, res)=>{
-    const {username, email, password} = req.body;
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
 
-    const hashedPassword = await bcrypt.hash(password, 10)
-
-
-    console.log(hashedPassword)
-
-//Create a new user and save it to DB
     const newUser = await prisma.user.create({
-        data:{
-            username,
-            email,
-            password: hashedPassword,
+      data: { username, email, password: hashedPassword },
+    });
 
-        }
-    })
-    // ({username, email, password: hashedPassword});
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
-}
+export const login = (req, res) => {
+  // Implement login logic here
+};
 
-export const login = (req, res)=>{
-    
-}
-
-export const logout = (req, res)=>{
-    
-}
+export const logout = (req, res) => {
+  // Implement logout logic here
+};
